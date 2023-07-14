33d7fb6d8001233b671f893bca925164b373c1ab
Anton <antonyadoval@gmail.com>
2023-07-14 20:33:17 +0300

    patch 5

--- app/user_generator/generate_users.py
+++ app/user_generator/generate_users.py
@@ -3,20 +3,21 @@ from typing import NamedTuple
 from collections.abc import Iterator
 
 faker=Faker()
-class User(NamedTuple):
+
+class user(NamedTuple):
     name: str
     email: str
 
-def generate_user()->User:
-    return User(
+def generate_user()->user:
+    return user(
         name= faker.first_name(),
         email= faker.email(),
     )
-def generate_users(amount: int =100) -> Iterator[User]:
+def generate_users(amount: int =100) -> Iterator[user]:
     for index in range(1, amount +1):
         yield generate_user()
 
 def print_users():
-    for index, user in enumerate(generate_users()):
-        string = (f'{index} Name: {user.name}, email: {user.email}')
-        print(string)
+   for index, user in enumerate(generate_users()):
+    string = (f'{index} Name: {user.name}, email: {user.email}')
+    print(string)
