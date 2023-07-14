4a9a7756044c796d30710c3d09f0bf84d537fc04
Anton <antonyadoval@gmail.com>
2023-07-14 20:26:42 +0300

    patch 4

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from app.user_generator import print_users
 from app.user_generator import generate_users
 from app.reading_a_file import reading_a_file
 def main():
+
     reading_a_file()
     print('\n')
 
-    users = generate_users()
-    print_users(users, True)
+    print_users()
     print('\n')
 
-    print('Who is There\n')
     astros()
     print('\n')
+
     average()
--- app/user_generator/generate_users.py
+++ app/user_generator/generate_users.py
@@ -1,11 +1,12 @@
 from faker import Faker
 from typing import NamedTuple
 from collections.abc import Iterator
+
+faker=Faker()
 class User(NamedTuple):
     name: str
     email: str
 
-faker=Faker()
 def generate_user()->User:
     return User(
         name= faker.first_name(),
@@ -15,8 +16,7 @@ def generate_users(amount: int =100) -> Iterator[User]:
     for index in range(1, amount +1):
         yield generate_user()
 
-
-def print_users(users):
-    for index, user in enumerate(users):
+def print_users():
+    for index, user in enumerate(generate_users()):
         string = (f'{index} Name: {user.name}, email: {user.email}')
         print(string)
